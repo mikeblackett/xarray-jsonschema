f8a34ada391174147d7daab499875cad674190c7
mikeblackett <dev@mikeblackett.uk>
2025-12-26 10:43:23 +0000

    Add generic type hints to `optional_type`

--- src/xarray_jsonschema/converters.py
+++ src/xarray_jsonschema/converters.py
@@ -1,17 +1,21 @@
 """This module provides custom `attrs` field converters"""
 
-from typing import Callable
+from typing import Callable, TypeVar
+
+TOptionalType = TypeVar('TOptionalType')
 
 
-def optional_type(converter: type) -> Callable:
+def optional_type(
+    converter: type[TOptionalType],
+) -> Callable[[object], TOptionalType | None]:
     """Optionally apply a type converter to the value.
 
-    If the `converter` can accept instances of `type`, this method is not needed. You can just use `attrs.converter.optional()`.
+    If the `converter` can accept instances of its own `type`, then you can just use `attrs.converter.optional()`. This method is primarily used for converting sub-model instances.
     """
 
-    def optional_converter(obj):
+    def optional_converter(obj: object) -> TOptionalType | None:
         if obj is None or isinstance(obj, converter):
             return obj
-        return converter(obj)
+        return converter(obj)  # type: ignore [reportCallIssue]
 
     return optional_converter
